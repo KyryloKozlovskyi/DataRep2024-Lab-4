fc49c00579dcf1e041335b361a9a4d1fbd0dc88c
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-18 10:18:25 +0000

    first commit

---
+++ src/components/Create.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Create component
+const Create = () => {
+    // Returns the relevant message
+    return (
+        <div>
+            <h3>Hello from the Create component</h3>
+        </div>
+    );
+}
+
+export default Create; // Exports the component
