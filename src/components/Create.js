40b84eef3bedd6a9ef9773fbd5e3619209274735
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-18 11:04:37 +0000

    Exercise 3.a, 3.b Completed. Create component inculed a form that uploads data to a server. Button click logs information submitted to the form to the console.

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Create component
 const Create = () => {
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title);
+    }
+
     // Returns the relevant message
     return (
         <div>
-            <h3>Hello from the Create component</h3>
+            <h2>This is my Create Component.</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Movie Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </div>
+                <input type="submit" value="Add Movie" />
+            </form>
         </div>
     );
 }
