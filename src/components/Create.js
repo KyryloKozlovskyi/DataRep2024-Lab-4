0312da7ad02d4fcc46a8c67fe44a8fd482a6b70d
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-18 11:22:06 +0000

    Comments added

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 // Create component
 const Create = () => {
     // Adds state variables to functional components
-    // Stores data returned from an API and manages the state of the application by updating the title
-    const [title, setTitle] = useState('');
-    const [year, setYear] = useState('');
-    const [poster, setPoster] = useState('');
+    // Stores data returned from an API and manages the state of the application.
+    const [title, setTitle] = useState(''); // manages title state
+    const [year, setYear] = useState(''); // manages year state
+    const [poster, setPoster] = useState(''); // manages poster state
 
     // Logs data submited to the form to the console
     const handleSubmit = (e) => {
@@ -20,19 +20,23 @@ const Create = () => {
             <h2>This is my Create Component.</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
+                    {/* Input box for movie title*/}
                     <label>Add Movie Title: </label>
                     <input type="text"
                         className="form-control"
                         value={title}
                         onChange={(e) => { setTitle(e.target.value) }}
                     />
+                    {/* Input box for movie year*/}
                     <label>Add Movie Year: </label>
                     <input type="text"
                         className="form-control"
                         value={year}
                         onChange={(e) => { setYear(e.target.value) }}
                     />
+                    {/* Input box for movie poster*/}
                     <label>Add Movie Poster: </label>
+                    {/* Input button */}
                     <input type="text"
                         className="form-control"
                         value={poster}
