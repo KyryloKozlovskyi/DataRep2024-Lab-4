4b9d531a048780a25be18be27e58ac3b1fd383ae
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-18 10:59:52 +0000

    Exercise 2.c, 2.d Completed. useEffect used to sync component, HTTP GET call returns JSON from API

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,38 +1,29 @@
 import React from 'react';
 import Movies from './Movies';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 // Read Component
 const Read = () => {
-    // Array of movie data stored in JSON
-    const data = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
+    // Adds state variables to functional components
+    // Stores data returned from an API and manages the state of the application.
+    const [movies, setMovies] = useState([]);
 
-    ]
+    // React hook, useEffect, to synchronize a component with an external system
+    // HTTP GET call returns the JSON data from
+    useEffect(() => {
+        axios.get("https://jsonblob.com/api/jsonblob/1287718524221775872").then((responce) => {
+            console.log(responce.data); // Log Obj to the console
+            setMovies(responce.data.movies) // 
+        }).catch()
+    }, []);
+
     // Returns the relevant message and the movie array
     return (
         <div>
             <h3>Hello from the Read component</h3>
-            <Movies myMovies={data} />
+            <Movies myMovies={movies} />
         </div>
     );
 }
